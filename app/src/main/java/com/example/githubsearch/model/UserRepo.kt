a921d0138ddcf58372ab9aeaba71692d7f411056
Lee-Jun-Young <leejy8506@naver.com>
2021-12-24 00:26:11 +0900

    detail repo webView를 위한 data class 추가

--- app/src/main/java/com/example/githubsearch/model/UserRepo.kt
+++ app/src/main/java/com/example/githubsearch/model/UserRepo.kt
@@ -3,6 +3,10 @@ package com.example.githubsearch.model
 import com.google.gson.annotations.SerializedName
 
 data class UserRepo(
+
+    @SerializedName("full_name")
+    val full_name: String,
+
     @SerializedName("name")
     val name: String,
 
