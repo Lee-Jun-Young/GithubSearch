5825aa477c35873a405f91644c319d40e52bf179
Lee-Jun-Young <leejy8506@naver.com>
2021-12-24 16:39:40 +0900

    okHttpClient() 추가

--- app/src/main/java/com/example/githubsearch/data/remote/RetrofitBuilder.kt
+++ app/src/main/java/com/example/githubsearch/data/remote/RetrofitBuilder.kt
@@ -2,8 +2,11 @@ package com.example.githubsearch.data.remote
 
 import com.example.githubsearch.BuildConfig
 import com.example.githubsearch.data.remote.api.GithubApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitBuilder {
 
@@ -11,8 +14,23 @@ class RetrofitBuilder {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient())
             .build()
             .create(GithubApi::class.java)
+    }
+
+    private fun okHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            interceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+
     }
 
 }
