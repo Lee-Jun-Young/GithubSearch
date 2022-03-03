4e97bb32aad339b1a509bcd8eedd45f5b4d53b13
Lee-Jun-Young <leejy8506@naver.com>
2022-03-03 09:30:33 +0900

    MainViewModel isBlank 변수 타입 변경

--- app/src/main/java/com/example/githubsearch/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/githubsearch/ui/main/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.githubsearch.ui.main
 
+import android.content.Context
+import android.content.res.Resources
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,12 +9,14 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.CombinedLoadStates
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.bumptech.glide.load.engine.Resource
+import com.example.githubsearch.R
 import com.example.githubsearch.data.repository.UserRepository
 import com.example.githubsearch.model.User
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(val repository: UserRepository) : ViewModel() {
+class MainViewModel @Inject constructor(private val repository: UserRepository) : ViewModel() {
 
     private var _data = MutableLiveData<Flow<PagingData<User>>>()
     val data: LiveData<Flow<PagingData<User>>> = _data
@@ -20,21 +24,16 @@ class MainViewModel @Inject constructor(val repository: UserRepository) : ViewMo
     private var _isEmpty = MutableLiveData<Boolean>()
     val isEmpty: LiveData<Boolean> = _isEmpty
 
-    private var _isBlank = MutableLiveData<String>()
-    var isBlank: LiveData<String> = _isBlank
+    private var _isBlank = MutableLiveData<Boolean>()
+    var isBlank: LiveData<Boolean> = _isBlank
 
     val searchId = MutableLiveData<String>()
 
     fun getUserId() {
         val userId = searchId.value
-
-        if (userId.isNullOrBlank()) {
-            _isBlank.value = "검색어를 입력해주세요."
-            return
-        }
+        _isBlank.value = userId.isNullOrBlank()
 
         _data.value = repository.getUserList(userId.toString(), 50).cachedIn(viewModelScope)
-
     }
 
     fun setUsersLoadState(loadState: CombinedLoadStates?) {
